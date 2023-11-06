b85b3c5b7b63b73288c1ab244333e551d0371878
Da1sypetals <da1sypetals.iota@gmail.com>
2023-11-06 10:44:56 +0800

    volume fixed

--- 103-lab01/Scripts/MomentOfInertiaUtils.cs
+++ 103-lab01/Scripts/MomentOfInertiaUtils.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using MatrixUtils;
@@ -12,40 +12,50 @@ using UnityEditor;
 using UnityEngine.UIElements;
 
 
-public static class MomentOfInertiaUtils {
+public static class MomentOfInertiaUtils
+{
 
 
-    private static float InertiaEntry(Vector3 a, Vector3 b, Vector3 c, Func<Vector3, float> func) {
-        return Matrix3.Columns(a, b, c).determinant / 20 * (func(a) + func(b) + func(c) + func(a + b + c));
+    private static float InertiaEntry(Vector3 a, Vector3 b, Vector3 c, Func<Vector3, float> func)
+    {
+        // volume / 20 == det(cols) / 120
+        return Matrix3.Columns(a, b, c).determinant / 120f * (func(a) + func(b) + func(c) + func(a + b + c));
     }
 
-    private static float Kxx(Vector3 vertex) {
+    private static float Kxx(Vector3 vertex)
+    {
         return vertex.y * vertex.y + vertex.z * vertex.z;
     }
-    
-    private static float Kyy(Vector3 vertex) {
+
+    private static float Kyy(Vector3 vertex)
+    {
         return vertex.x * vertex.x + vertex.z * vertex.z;
     }
-    
-    private static float Kzz(Vector3 vertex) {
+
+    private static float Kzz(Vector3 vertex)
+    {
         return vertex.x * vertex.x + vertex.y * vertex.y;
     }
-    
-    private static float Kxy(Vector3 vertex) {
+
+    private static float Kxy(Vector3 vertex)
+    {
         return vertex.x * vertex.y;
     }
-    
-    private static float Kyz(Vector3 vertex) {
+
+    private static float Kyz(Vector3 vertex)
+    {
         return vertex.y * vertex.z;
     }
-    
-    private static float Kzx(Vector3 vertex) {
+
+    private static float Kzx(Vector3 vertex)
+    {
         return vertex.z * vertex.x;
     }
 
-    public static Matrix3 MomentOfInertiaTensor(Vector3 a, Vector3 b, Vector3 c) {
+    public static Matrix3 MomentOfInertiaTensor(Vector3 a, Vector3 b, Vector3 c)
+    {
         // suppose mass center at (0, 0, 0), i.e. all vertices are relative coords
-        
+
         float kxx = InertiaEntry(a, b, c, Kxx);
         float kyy = InertiaEntry(a, b, c, Kyy);
         float kzz = InertiaEntry(a, b, c, Kzz);
@@ -65,6 +75,6 @@ public static class MomentOfInertiaUtils {
             kzz
         );
     }
-    
-    
+
+
 }
